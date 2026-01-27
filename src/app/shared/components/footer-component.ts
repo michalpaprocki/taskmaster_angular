cf5d443d3680c2f8c37ed5b597e5a94ffcb24e9b
michalpaprocki <michalpaprocki@proton.me>
2026-01-27 15:03:13 +0100

    create footer markup

---
+++ src/app/shared/components/footer-component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'footer-component',
+    templateUrl: 'footer-component.html',
+    imports: [],
+    styleUrl: './footer-component.scss'
+})
+
+export class FooterComponent {
+
+}
