19a6c50eb96743160ab3c929c6b8540a5d107dcc
michalpaprocki <michalpaprocki@proton.me>
2026-01-27 14:59:44 +0100

    clear out app markup and fetch user in app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: `<router-outlet/>`
 })
 export class App {
   protected readonly title = signal('taskmaster_fe');
+ private auth = inject(AuthService);
+  private platformId = inject(PLATFORM_ID);
+
+  constructor() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.auth.hasValidSession().subscribe({
+        next: valid => {
+          if (valid) {
+            this.auth.fetchUser().subscribe();
+          }
+        },
+        error: () => {
+          this.auth.clearUser();
+        }
+      });
+    }
+  }
 }
