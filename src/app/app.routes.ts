408f5efefd3f4cb580a6100a32debfaed938882c
michalpaprocki <michalpaprocki@proton.me>
2026-01-27 15:00:52 +0100

    edit app routes to include main layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
-export const routes: Routes = [];
+
+import { HomePage } from './features/home/home-page';
+
+
+export const routes: Routes = [
+    {path: "", component: MainLayout, children: [
+        {path: "", component: HomePage}
+    ]}
+];
