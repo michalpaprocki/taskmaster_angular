6a17e83b6cb5c9f9fff80e07305b779f2756167c
michalpaprocki <michalpaprocki@proton.me>
2026-01-31 08:37:11 +0100

    moved task form to separate page

--- src/app/features/tasks/tasks-page.ts
+++ src/app/features/tasks/tasks-page.ts
@@ -1,7 +1,7 @@
 import { NgIf } from "@angular/common";
 import { Component, inject, signal } from "@angular/core";
-import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
-import { Router, RouterLinkWithHref } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterLinkWithHref } from "@angular/router";
 import { AuthService } from "../../core/services/auth.service";
 
 
@@ -17,18 +17,10 @@ import { AuthService } from "../../core/services/auth.service";
 export class TasksPage {
 constructor(){}
     errMessage = signal('');
-    // user = signal<any | null>(null);
     defaultDeadline = this.getTomorow()
-    private formBuilder = inject(FormBuilder);
-    private router = inject(Router);
+
     private auth = inject(AuthService);
-
-
-    createTaskForm = this.formBuilder.group({
-        title: ['', [Validators.required, Validators.minLength(5)]],
-        description: ['', [Validators.required, Validators.minLength(10)]],
-        deadline: [this.getTomorow(), Validators.required]
-    })
+  
 
     onSubmit() {
     this.errMessage.set('');
