66fb0383f1e58c3f56583bdda68bbee080fc81ff
michalpaprocki <michalpaprocki@proton.me>
2026-01-31 08:24:32 +0100

    create task service

---
+++ src/app/core/services/task.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, signal } from "@angular/core"
+import { environment } from "../../../environments/environment";
+import { Task } from "../../models/task.model";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+    BE_URL = environment.BE_URL
+    loaded = signal(false)
+    loading = signal(false)
+    tasks = signal<Task[]>([])
+    constructor(private http:HttpClient){}
+
+    loadTasks() {
+    if(this.loaded() || this.loading()) return
+
+    this.loading.set(true)
+    this.fetchTasks().subscribe({
+        next: tasks => {
+            console.log(tasks)
+            this.tasks.set(tasks),
+            this.loaded.set(false)
+        },
+        error: () => {
+            this.loading.set(false)
+        },
+        complete: () => [
+            this.loading.set(true)
+        ]
+        })
+    }
+    createTask(task: Task) {
+        console.log(task)
+        return this.http.post<Task>(this.BE_URL+"/api/task", task, {withCredentials: true})
+
+    }
+    fetchTasks(){
+        return this.http.get<Task[]>(this.BE_URL+"/api/tasks", {withCredentials: true})
+    }
+    fetchTask(task: Task) {
+        return this.http.get<Task>(this.BE_URL+"/api/task/"+task.id, {withCredentials: true})
+    }
+    getAllTasks() {
+        if (this.loaded() && !this.loading()){
+            return this.tasks();
+        }
+
+        return this.tasks();
+    }
+    deleteTask(task: Task) {
+        return this.http.delete<Task>(this.BE_URL+"/api/task/"+task.id, {withCredentials: true})
+    }
+}
