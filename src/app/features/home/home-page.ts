63b7d36e51aca3da9f084c3d99b663b0a40ec6e6
michalpaprocki <michalpaprocki@proton.me>
2026-01-31 08:29:47 +0100

    home template update

--- src/app/features/home/home-page.ts
+++ src/app/features/home/home-page.ts
@@ -1,12 +1,11 @@
-import { Component, effect, OnInit, PLATFORM_ID, signal, inject } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { RouterLinkWithHref } from "@angular/router";
 import { AuthService } from "../../core/services/auth.service";
 
-import { isPlatformBrowser, NgIf } from "@angular/common";
 
 @Component({
   selector: 'home-page',
-  imports: [RouterLinkWithHref, NgIf],
+  imports: [RouterLinkWithHref],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
     standalone: true,
