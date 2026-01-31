af092e413c264aa7fafcabcccb3a284dc97df7eb
michalpaprocki <michalpaprocki@proton.me>
2026-01-31 08:26:10 +0100

    helper fns for models

---
+++ src/app/utils/model.utils.ts
@@ -0,0 +1,26 @@
+import { Organization } from "../models/organization.model";
+import { Task } from "../models/task.model";
+
+
+export const createOrganization = (organization:Partial<Organization>):Organization => {
+    return {
+        id: organization.id ?? "",
+        description: organization.description ?? '',
+        name: organization.name ?? '',
+        memberships: organization.memberships ?? [],
+        createdAt: organization.createdAt ?? new Date(),
+        updatedAt: organization.updatedAt ?? new Date(),
+        members: organization.members ?? []
+        }
+    }
+export const createTask = (task: Partial<Task>):Task => {
+    return {
+        id: task.id ?? "",
+        assignedOrgs: task.assignedOrgs ?? [],
+        title: task.title ?? "",
+        deadline: task.deadline ?? new Date(),
+        description: task.description ?? "",
+        createdAt: task.createdAt ?? new Date(),
+        updatedAt: task.updatedAt ?? new Date(),
+    }
+}
