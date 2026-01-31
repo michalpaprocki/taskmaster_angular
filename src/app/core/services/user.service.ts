4e1849486b430cc64c1a279acaf6f4394d39dbce
michalpaprocki <michalpaprocki@proton.me>
2026-01-31 08:24:56 +0100

    created user service

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { User } from "../../models/user.model";
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+    BE_URL = environment.BE_URL
+
+    constructor(private http:HttpClient){}
+
+    getUsers(){
+        return this.http.get<User[]>(this.BE_URL+"/api/users", { withCredentials: true })
+    }
+}
