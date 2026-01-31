b52a7fc7c157b399c75223a52bbe19bc0bdddcdc
michalpaprocki <michalpaprocki@proton.me>
2026-01-31 08:22:28 +0100

    added multiple fns to authservice

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,27 +1,65 @@
 import { HttpClient } from '@angular/common/http';
-import { computed, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
+import { catchError, filter, finalize, map, Observable, of, take, tap, throwError } from 'rxjs';
 import { User } from '../../models/user.model';
+import { environment } from '../../../environments/environment';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   
-  private BE_URI = 'http://localhost:8080'
+  http = inject(HttpClient)
+  router = inject(Router)
+  private BE_URI = environment.BE_URL
 
   private _user = signal<User | null>(null)
   public readonly user = computed(() => this._user())
   public readonly isAuthenticated = computed(() => !!this._user())
-  constructor(private router: Router, private http: HttpClient) {
+  authChecked = signal(false)
+  loading = signal(false)
 
-  }
+ loadUser() {
+    // check if auth request completed
+        if(this.authChecked()){
+            return of(this._user())
+        }
+      // check if request in flight
+        if(this.loading()) {
+          
+          return toObservable(this.authChecked).pipe(
+            // wait for authChecked == true
+            filter(Boolean),
+            // stop after 1st occurance
+            take(1),
+            // return the value when fetch completes
+            map(() => this._user())
+          )
+        }
+
+        this.loading.set(true)
+
+        return this.fetchUser().pipe(
+          tap(user => this._user.set(user)),
+          catchError(() => {
+            this._user.set(null)
+            return of(null)
+          }),
+          finalize(() => {
+            this.authChecked.set(true)
+            this.loading.set(false)
+          })
+        )
+    }
+
   hasValidSession():Observable<boolean> {
     return this.http.get(this.BE_URI+"/auth/session", { withCredentials: true }).pipe(
       map(() => true),
       catchError(err => {
         if(err.status == 401) {
+          
           this.clearUser()
           return of(false)
         }
@@ -33,7 +71,7 @@ export class AuthService {
     return this.http.post(this.BE_URI + '/auth/register', { name, email, password }, { withCredentials: true })
   }
   login(email: string, password: string) {
-    return this.http.post(`${this.BE_URI}/auth/login`, { email, password }, { withCredentials: true })
+    return this.http.post(this.BE_URI+"/auth/login", { email, password }, { withCredentials: true })
   }
   rawHttpFetchUser() {
      return this.http.get<User>(this.BE_URI+"/api/users/me", {withCredentials: true})
@@ -49,15 +87,20 @@ export class AuthService {
     )
   }
   clearUser(){
+
     this._user.set(null);
     this.router.navigate(['/']);
   }
   logout() {
     this.clearUser()
-    this.http.post(`${this.BE_URI}/auth/logout`, {}, { withCredentials: true }).subscribe();
+    this.http.post(this.BE_URI+ "/auth/logout", {}, { withCredentials: true }).subscribe();
   }
 
   refresh() {
     return this.http.post(this.BE_URI+"/auth/refresh", {}, {withCredentials: true})
   }
+  readonly authState = computed(() => {
+    if (!this.authChecked()) return "loading"
+    return this._user() ? "authenticated" : "unauthenticated"
+  })
 }
