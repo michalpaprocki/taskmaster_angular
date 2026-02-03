8c09333c2f236f3bb23178c27169121264dc23b7
michalpaprocki <michalpaprocki@proton.me>
2026-02-03 17:50:47 +0100

    edit server routes for client rendered routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,25 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+      {
+        path: 'tasks/edit/:slug',
+        renderMode: RenderMode.Client
+    },
+      {
+        path: 'tasks/:slug',
+        renderMode: RenderMode.Client
+    },
+    {
+        path: 'organizations/edit/:slug',
+        renderMode: RenderMode.Client
+    },
+    {
+        path: 'organizations/:slug',
+        renderMode: RenderMode.Client
+    },
   {
     path: '**',
     renderMode: RenderMode.Prerender
   }
+  
 ];
