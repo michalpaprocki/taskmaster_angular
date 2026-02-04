c7af6a53bf7e99e096aec5ece6ef43cce64600de
michalpaprocki <michalpaprocki@proton.me>
2026-02-04 11:33:47 +0100

    update slug-task-page to show assigned organizations

--- src/app/features/tasks/slug/slug-task-page.ts
+++ src/app/features/tasks/slug/slug-task-page.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit, signal } from "@angular/core";
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { Task } from "../../../models/task.model";
+import { Organization } from "../../../models/organization.model";
 import { TaskService } from "../../../core/services/task.service";
 import { createTask } from "../../../utils/model.utils";
 
@@ -8,19 +9,20 @@ import { createTask } from "../../../utils/model.utils";
     selector: 'slug-task-page',
     styleUrl: 'slug-task-page.scss',
     templateUrl: 'slug-task-page.html',
-    imports: [],
+    imports: [RouterLink],
     standalone: true
 })
 
 export class SlugTaskPage implements OnInit{
     router = inject(Router)
     route = inject(ActivatedRoute)
-    taskService = inject(TaskService)
+    taskService = inject(TaskService)   
+
 
     errMessage = signal('')
     task = signal<Task|null>(null)
     taskSlug = signal<string|null>(null)
-
+    showDetails = signal(false)
     ngOnInit(): void {
         this.taskSlug.set(this.route.snapshot.paramMap.get('slug'))
         const task = createTask({id: this.route.snapshot.paramMap.get('slug')!})
@@ -28,5 +30,14 @@ export class SlugTaskPage implements OnInit{
             next: task => {this.task.set(task)},
             error: err => this.errMessage.set(err.error?.message)
             })
+    }
+    toggleDetails() {
+        this.showDetails.set(!this.showDetails())
+    }
+
+     get assignedOrganizations(): Organization[] {
+        return (this.task()?.assignedOrganizations ?? []).filter(
+            (o) : o is Organization => typeof o != 'string'
+        )
     }
 }
